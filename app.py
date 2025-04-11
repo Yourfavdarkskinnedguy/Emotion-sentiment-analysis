c43fea2b8994ca05908fb8063c20d08c6dc68083
sholademi daniel <sholademidaniel@gmail.com>
2025-04-11 18:28:58 +0100

    change

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request, jsonify
-import cv2
 import numpy as np
+import cv2
 from PIL import Image
-import os
 from ultralytics import YOLO
+import os
 
-model_filepath= os.path.join(os.getcwd(), "model", "best.pt")
-print(f"model file path is {model_filepath}")
-
-model= YOLO(model_filepath)
 app = Flask(__name__)
+model_filepath= os.path.join(os.getcwd(), "model", "best.pt")
+model = YOLO(model_filepath) 
 
 @app.route('/')
 def index():
@@ -20,17 +18,25 @@ def predict():
     file = request.files['frame']
     image = Image.open(file.stream).convert('RGB')
     img_array = np.array(image)
-
     img_bgr = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
 
-    results = model(img_bgr)[0]  # Get first result
+    results = model(img_bgr)[0]
+    detections = []
 
-    if not results.boxes:
-        return jsonify({'emotion': None})
+    if results.boxes is not None and len(results.boxes) > 0:
+        for box in results.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+            cls_id = int(box.cls[0])
+            label = results.names[cls_id]
+            detections.append({
+                'x1': x1,
+                'y1': y1,
+                'x2': x2,
+                'y2': y2,
+                'label': label
+            })
 
-    pred = results.names[int(results.boxes.cls[0])]
-    return jsonify({'emotion': pred})
-
+    return jsonify({'detections': detections})
 
 if __name__ == '__main__':
     app.run(debug=True)
